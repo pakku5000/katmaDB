8dc768bedc084aca04db4a5e7c46043df75253bc
Michael Kuhn <pakku5000@gmail.com>
2022-08-31 15:43:50 +0200

    Old version/comments deleted, cleaned up code

--- app.js
+++ app.js
@@ -1,43 +1,11 @@
 const mongoose = require("mongoose")
 const { userSchema , userModel } = require('./models.js');
-//const { MongoClient } = require('mongodb');
-// or as an es module:
-// import { MongoClient } from 'mongodb'
 
-/*// Connection URL
-const url = 'mongodb://localhost:27017';
-*/
-
-/* The non-mongoose client constant
-const client = new MongoClient(url);
-*/
-/*// Database Name
-const dbName = 'katmaDB';
-*/
-/* 
-non-mongoose connect:
-
-async function main() {
-  // Use connect method to connect to the server
-  
+// Connection URL: 'mongodb://localhost:27017';
 
 
-  await client.connect();
-  console.log('Connected successfully to server');
-  const db = client.db(dbName);
-  const userData = db.collection('userData');
 
-  // the following code examples can be pasted here...
-
-  return 'done.';
-}
-*/
-
-/* Adding Documents to the Collection: userData
-   _id is the randomly generated Object ID, the userId is a randomized String, the date is the current Date, 
-   randomInt is a random integer between 0 and 100. It is possible to add the key: "text" with a string in the Document.
-   _id, userID and date are required, randomInt and text aren't required.*/
-
+// Database Name: katmaDB
 
 /*Connection to Localhost server, to katmaDB*/
 const mongoConnect = mongoose.connect('mongodb://127.0.0.1:27017/katmaDB')
@@ -46,7 +14,12 @@ const mongoConnect = mongoose.connect('mongodb://127.0.0.1:27017/katmaDB')
   console.log(`OH NO! MONGO CONNECTION ERROR!`);
   console.log(err);
 })
-var katmaDB = mongoose.connection
+var katmaDB = mongoose.
+
+/* Adding Documents to the Collection: userData
+   _id is the randomly generated Object ID, the userId is a randomized String, the date is the current Date, 
+   randomInt is a random integer between 0 and 100. It is possible to add the key: "text" with a string in the Document.
+   _id, userID and date are required, randomInt and text aren't required.*/
 
 setInterval(async function dbFiller(){
   const userdoc = {
@@ -60,10 +33,3 @@ setInterval(async function dbFiller(){
     console.log("A document was inserted with the userId: " + user.userId);
   });  
 }, 5000);
-
-
-/*main()
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => client.close());
-*/
