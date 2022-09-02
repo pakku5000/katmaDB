ab64a363c7129b3019570a3bff5852798ca33460
Michael Kuhn <pakku5000@gmail.com>
2022-09-02 17:24:00 +0200

    bugfix, file users.js was not added to git

---
+++ users.js
@@ -0,0 +1,18 @@
+const { default: mongoose, models } = require("mongoose");
+
+//const id = mongoose.Types.ObjectId();
+
+//Schema for the required/allowed keys for a user
+const userSchema = new mongoose.Schema({
+    userId : {type : String, required : true},
+    date : {type : Date, required : true},
+    randomInt : {type : Number, required : false},
+    text : {type : String, required : false}
+});
+
+//The model is like a Class to make users with the above defined Schema
+const userModel = mongoose.model("users", userSchema);
+
+module.exports.userSchema = userSchema;
+module.exports.userModel = mongoose.model('users', userSchema);
+
